e76908873a754265d8efe3b32f6feaa237628798
HappyLeslieAlexander <HappyLeslieAlexander@duck.com>
2024-07-09 18:43:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = '07f1acea-1a86-175a-01fb-7fbe23ff10bb';
+let userID = '7597a4e5-bee0-470d-b50d-ebf412c00b47';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
